913e8377ec79aacf6e7a1a556eede2ce8f5e6170
Venkat-187 <2420030187@klh.edu.in>
2025-07-29 13:30:17 +0530

    Practical_Task 1

---
+++ practicaltaskone/Enhanced_Forloop.java
@@ -0,0 +1,13 @@
+package practicaltaskone;
+
+public class Enhanced_Forloop {
+
+	public static void main(String[] args) {
+		int[] speed= {120,85,45,60};
+		for(int i:speed) {
+			System.out.println(i+" kmph");
+		}
+
+	}
+
+}
